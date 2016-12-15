65e0da854a5bed75429e8a7301e59d421ebead57
satellite107 <kalicki.paul@icloud.com>
2016-12-15 02:16:14 +0100

    Add first program -> Hello World in Swift

---
+++ hello_world_swift/hello_world_swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  hello_world_swift
+//
+//  Created by Paweł Kalicki on 15/12/2016.
+//  Copyright © 2016 Paweł Kalicki. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
