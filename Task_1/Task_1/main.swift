b1096d065ee1e19f1e55fd288ed938f6e592d52a
satellite107 <kalicki.paul@icloud.com>
2016-12-19 15:26:07 +0100

    Add first task_1. I make class with initialization method and description method

---
+++ Task_1/Task_1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Task_1
+//
+//  Created by Paweł Kalicki on 15/12/2016.
+//  Copyright © 2016 Paweł Kalicki. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
