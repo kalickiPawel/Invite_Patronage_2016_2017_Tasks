2aced9c2d1ab608dbe43bc9defa42c58a759f431
satellite107 <kalicki.paul@icloud.com>
2017-01-20 20:16:56 +0100

    Edycja programu zastosowanie funkcji

--- Task_3/Task_3/main.swift
+++ Task_3/Task_3/main.swift
@@ -8,21 +8,28 @@
 
 import Foundation
 
-func dzielenieTextu(){
-    
+func dzielenieDanych(wprowadzanyText: String?) -> Float{
+    let pudelko = wprowadzanyText?.components(separatedBy: " ")
+    for i in pudelko!{
+        var wymiaryPudelka = i.components(separatedBy: "x")
+        let wysokosc: Float = Float(wymiaryPudelka[0])!
+        let szerokosc: Float = Float(wymiaryPudelka[1])!
+        let dlugosc: Float = Float(wymiaryPudelka[2])!
+}
+
+func ileSznurka(wprowadzanyText: String?) -> Float
+{
+   dzielenieDanych()
 }
 print("Wprowadź długości pudełka. Rozmiary są w formie „wysokość x szerokość x długość”, a poszczególne pudełka są rozdzielone spacjami, np. „2x3x4 1x2x3 2x2x2”")
-
+    
 let wprowadzanyText: String? = readLine(strippingNewline: true)
-var wprowadzanyTextTab = wprowadzanyText?.components(separatedBy: " ")
-var i: Int = 0
-for i in wprowadzanyTextTab!.count{
-    var zmienne = wprowadzanyTextTab[i]?.components(separatedBy: "x")
-    let wysokosc: Float = Float(zmienne![0])!
-    let szerokosc: Float = Float(zmienne![1])!
-    let dlugosc: Float = Float(zmienne![2])!
-    let poleBezOwijania: Float = 2*wysokosc*szerokosc+2*wysokosc*dlugosc+2*szerokosc*dlugosc
-    var namniejszePole: Float
+
+dzielenieDanych(wprowadzanyText)
+    
+var sumaPol: Float = 0
+let poleBezOwijania: Float = 2*wysokosc*szerokosc+2*wysokosc*dlugosc+2*szerokosc*dlugosc
+var namniejszePole: Float
     if(wysokosc*szerokosc < wysokosc*dlugosc || wysokosc*szerokosc < szerokosc*dlugosc)
     {
         namniejszePole = wysokosc*szerokosc
@@ -35,9 +42,13 @@ for i in wprowadzanyTextTab!.count{
     {
         namniejszePole = szerokosc*dlugosc
     }
-    let polePudelka = poleBezOwijania+namniejszePole
-    print(polePudelka)
+    
+        let polePudelka = poleBezOwijania+namniejszePole
+        sumaPol+=polePudelka
+        print("Pole pudelka o wymiarach: "+i+":")
+        print(polePudelka)
+    }
+    print(sumaPol)
 }
-
 
 
