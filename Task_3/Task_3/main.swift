0c881943ad7c7e803482b57579ee3767b062385f
satellite107 <kalicki.paul@icloud.com>
2017-01-20 20:25:02 +0100

    Wracam do poprzedniej wersji, praca nad funkcją sznurka

--- Task_3/Task_3/main.swift
+++ Task_3/Task_3/main.swift
@@ -8,28 +8,21 @@
 
 import Foundation
 
-func dzielenieDanych(wprowadzanyText: String?) -> Float{
-    let pudelko = wprowadzanyText?.components(separatedBy: " ")
-    for i in pudelko!{
-        var wymiaryPudelka = i.components(separatedBy: "x")
-        let wysokosc: Float = Float(wymiaryPudelka[0])!
-        let szerokosc: Float = Float(wymiaryPudelka[1])!
-        let dlugosc: Float = Float(wymiaryPudelka[2])!
-}
-
-func ileSznurka(wprowadzanyText: String?) -> Float
-{
-   dzielenieDanych()
-}
 print("Wprowadź długości pudełka. Rozmiary są w formie „wysokość x szerokość x długość”, a poszczególne pudełka są rozdzielone spacjami, np. „2x3x4 1x2x3 2x2x2”")
-    
-let wprowadzanyText: String? = readLine(strippingNewline: true)
-
-dzielenieDanych(wprowadzanyText)
-    
 var sumaPol: Float = 0
-let poleBezOwijania: Float = 2*wysokosc*szerokosc+2*wysokosc*dlugosc+2*szerokosc*dlugosc
-var namniejszePole: Float
+let wprowadzanyText: String? = readLine(strippingNewline: true)
+let pudelko = wprowadzanyText?.components(separatedBy: " ")
+
+for i in pudelko!{
+    var wymiaryPudelka = i.components(separatedBy: "x")
+        
+    let wysokosc: Float = Float(wymiaryPudelka[0])!
+    let szerokosc: Float = Float(wymiaryPudelka[1])!
+    let dlugosc: Float = Float(wymiaryPudelka[2])!
+    
+    let poleBezOwijania: Float = 2*wysokosc*szerokosc+2*wysokosc*dlugosc+2*szerokosc*dlugosc
+    var namniejszePole: Float
+        
     if(wysokosc*szerokosc < wysokosc*dlugosc || wysokosc*szerokosc < szerokosc*dlugosc)
     {
         namniejszePole = wysokosc*szerokosc
@@ -42,13 +35,9 @@ var namniejszePole: Float
     {
         namniejszePole = szerokosc*dlugosc
     }
-    
-        let polePudelka = poleBezOwijania+namniejszePole
-        sumaPol+=polePudelka
-        print("Pole pudelka o wymiarach: "+i+":")
-        print(polePudelka)
-    }
-    print(sumaPol)
+        
+    let polePudelka = poleBezOwijania+namniejszePole
+    sumaPol+=polePudelka
+    print("Pole pudelka o wymiarach: "+i+":")
+    print(polePudelka)
 }
-
-
