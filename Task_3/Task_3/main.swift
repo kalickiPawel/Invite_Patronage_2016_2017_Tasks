165e5845f308cf3673ae55285528f4b8111653ea
satellite107 <kalicki.paul@icloud.com>
2017-01-27 23:24:05 +0100

    Change functions

--- Task_3/Task_3/main.swift
+++ Task_3/Task_3/main.swift
@@ -10,26 +10,36 @@ import Foundation
 
 func calcLengthString(x: Float, y: Float, z: Float) -> Float {
     
-    var minCircuit: Float = 0
+    let circuits = [2*x+2*y,2*x+2*z,2*y+2*z]
     
-    if(2*x+2*y < 2*x+2*z || 2*x+2*y < 2*y+2*z)
-    {
-        minCircuit = 2*x+2*y
-    }
-    else if(2*x+2*z < 2*x+2*y || 2*x+2*z < 2*y+2*z)
-    {
-        minCircuit = 2*x+2*z
-    }
-    else
-    {
-        minCircuit = 2*y+2*z
-    }
-    
-    let tie: Float = minCircuit
+    let minCircuit: Float = circuits.min()!
+
     let knot: Float = x*y*z
-    let cord: Float = tie + knot
+    let cord: Float = minCircuit + knot
     
     return cord
+}
+
+func calcMinArea(x: Float, y: Float, z: Float) -> Float {
+    
+    let walls = [x*y,x*z,y*z]
+    
+    var withoutWrap: Float = 0
+    
+    for i in 1...walls.count-1
+    {
+        withoutWrap += 2*walls[i]
+    }
+    
+    let wynik: Float = withoutWrap+walls.min()!
+    
+    return wynik
+}
+
+struct Dimension {
+    var width : Float
+    var height : Float
+    var depth : Float
 }
 
 print("Wprowadź długości pudełka. Rozmiary są w formie „wysokość x szerokość x długość”, a poszczególne pudełka są rozdzielone spacjami, np. „2x3x4 1x2x3 2x2x2”: ")
@@ -37,41 +47,18 @@ print("Wprowadź długości pudełka. Rozmiary są w formie „wysokość x s
 let text: String? = readLine(strippingNewline: true)
 let box = text?.components(separatedBy: " ")
 
-var minArea: Float
 var areasSum: Float = 0
-var lengthString: Float = 0
 var cordsSum: Float = 0
 
 for i in box!{
     
     var boxDimensions = i.components(separatedBy: "x")
-        
-    let x: Float = Float(boxDimensions[0])!
-    let y: Float = Float(boxDimensions[1])!
-    let z: Float = Float(boxDimensions[2])!
     
-    let withoutWrap: Float = 2*x*y+2*x*z+2*y*z
+    var dimensions = Dimension(width: Float(boxDimensions[0])!, height: Float(boxDimensions[1])!, depth: Float(boxDimensions[2])!)
     
-    if(x*y < x*z || x*y < y*z)
-    {
-        minArea = x*y
-    }
-    else if(x*z < x*y || x*z < y*z)
-    {
-        minArea = x*z
-    }
-    else
-    {
-        minArea = y*z
-    }
-    
-    let boxArea: Float = withoutWrap+minArea
-    
-    areasSum += boxArea
-    
-    lengthString = calcLengthString(x: x, y: y, z: z)
-    
-    cordsSum += lengthString
+    areasSum += calcMinArea(x: dimensions.width, y: dimensions.height, z: dimensions.depth)
+    cordsSum += calcLengthString(x: dimensions.width, y: dimensions.height, z: dimensions.depth)
 }
+
 print("Suma pól jest równa: "+String(areasSum))
 print("Suma długości sznurka jest równa: "+String(cordsSum))
